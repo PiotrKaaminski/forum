fae448b3bb107cc1a24ec9df158859fd580962f4
Piotr Kamiński <piotr.kaminski@billongroup.com>
2026-04-02 09:49:50 +0200

    initial commit

---
+++ src/test/java/pl/kaminski/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kaminski.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
