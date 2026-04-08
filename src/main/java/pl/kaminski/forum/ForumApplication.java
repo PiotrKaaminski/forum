ad674b5b2688ec29953323bf7be2519840bb840d
Piotr Kamiński <piotr.kaminski@billongroup.com>
2026-04-08 12:33:27 +0200

    update

--- src/main/java/pl/kaminski/forum/ForumApplication.java
+++ src/main/java/pl/kaminski/forum/ForumApplication.java
@@ -2,12 +2,13 @@ package pl.kaminski.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import pl.kaminski.forum.category.infrastructure.CategoryConfiguration;
 import pl.kaminski.forum.commons.CommonsConfiguration;
 import pl.kaminski.forum.users.infrastructure.UserConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @Import({UserConfiguration.class, CommonsConfiguration.class, CategoryConfiguration.class})
 public class ForumApplication {
 
