e5d36ac5cd74ba83820f1dc08f7c840ffb187fca
Piotr Kamiński <piotr.kaminski@billongroup.com>
2026-04-21 09:51:07 +0200

    update

--- src/test/java/pl/kaminski/forum/users/infrastructure/UserRepositoryIntegrationTest.java
+++ src/test/java/pl/kaminski/forum/users/infrastructure/UserRepositoryIntegrationTest.java
@@ -5,18 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class UserRepositoryIntegrationTest {
 
-    @Autowired
-    UserConfiguration userConfiguration;
-
     UserRepository userRepository;
-
-    @BeforeEach
-    void setUp() {
-        userRepository = userConfiguration.userRepository;
-    }
 
     @Test
     void test() {
-        userRepository.fdsafds
     }
 }
